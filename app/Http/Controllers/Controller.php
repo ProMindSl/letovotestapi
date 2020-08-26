cc3f8d489d92f44b87cb0a32d7edb9cbdd22310c
ProMindSl <hot_peachy@hotmail.com>
2020-08-26 20:27:36 +0300

    swagger added to dependences

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @SWG\Swagger(
+ *   schemes={"http"},
+ *   host="promindsl.ru",
+ *   basePath="/",
+ *   @SWG\Info(
+ *     title="Letovo test API",
+ *     version="1.0.0"
+ *   )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
